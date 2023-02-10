633a6b92eafaa7d6baf4f2356f92b491f9b599e8
Bread <breadwheathead@gmail.com>
2023-02-10 15:49:37 +0700

    lesson 1: all tascks completed

---
+++ lesson_1/task_2.js
@@ -0,0 +1,9 @@
+// 2. Работа с переменными.
+
+var message = "Hello! Wat's up?";
+var number = 42;
+var choice = true;
+
+console.log(message + ': ' + typeof message);
+console.log(number + ': ' + typeof number);
+console.log(choice + ': ' + typeof choice);
